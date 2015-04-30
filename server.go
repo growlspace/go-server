f3d75529f2846ced954162522e390ce3d66b39f1
Nicholas Tinsley <00sturm@gmail.com>
2015-04-30 15:10:58 -0400

    reading from DB working

--- server.go
+++ server.go
@@ -1,15 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"github.com/codegangsta/martini"
 	"net/http"
+	"fmt"
+	"time"
+	"database/sql"
+	_ "github.com/lib/pq"
 )
+func SetupDB() *sql.DB {
+	db, err := sql.Open("postgres", "user=postgres password=passwordlol dbname=postgres sslmode=disable")
+	PanicIf(err)
+	return db
+}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func PanicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	m := martini.Classic()
+
+	m.Map(SetupDB())
+
+	m.Get("/", func(rw http.ResponseWriter, r *http.Request, db *sql.DB) {
+		rows, err := db.Query("SELECT * FROM users")
+		PanicIf(err)
+		defer rows.Close()
+		var id, username, password, name, bio string
+		var created, updated time.Time
+		for rows.Next() {
+			err := rows.Scan(&id, &username, &password, &name, &bio, &created, &updated)
+			PanicIf(err)
+
+			fmt.Fprintf(rw, "ID: %s\nUser: %s\nPassword: %s\nName: %s\nBio: %s\nCreated: %s\nUpdated: %s\n", id, username, password, name, bio, created, updated)
+		}
+	})
+
+	m.Run()
 }
